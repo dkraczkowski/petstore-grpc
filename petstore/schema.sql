cb140e9fa122171d57f4c4b7af3ffe964e896899
Dawid Kraczkowski <dawid.kraczkowski@gmail.com>
2019-11-19 19:11:57 +0100

    Pet store proto files

---
+++ petstore/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS categories (
+  category_id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+  name char(128) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS pets (
+  pet_id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+  category_id integer NOT NULL,
+  status integer(1) NOT NULL,
+  name char(128) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS pet_photos (
+  pet_image_id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+  pet_id integer NOT NULL,
+  name char(128),
+  url char(128) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS customers (
+  customer_id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+  first_name char(128) NOT NULL,
+  last_name char(128) NOT NULL,
+  email char(128) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS orders (
+  order_id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+  customer_id integer NOT NULL,
+  pet_id integer NOT NULL,
+  status integer NOT NULL DEFAULT(0)
+);
